59074e8a0535646f87490ecdefb674aabe812766
Maidele Archelus <maidele@usf.edu>
2025-02-11 16:34:36 -0500

    update task 2-4

--- cc_example2.js
+++ cc_example2.js
@@ -6,6 +6,42 @@ let products = [
 
 ];
 
-getProductsByCategory(products, "Electronics");{
-console.log(getProductsByCategory) (products, "Electronics")
+function getProductsByCategory(products_array, category_name);{
+    return products_array.filter(product => product.category == category == category_name);
+    
+//}
+
+console.log(getProductsByCategory(products, "Electronics"));
+
+//Task 2
+
+function applyDiscount(products, discountRate) {
+ return products.map(products => ({
+    ...product,
+    price: products.price -(prodcut.price *discountRate)
+} 
+
+ ))
 }
+
+ console.log (applyDiscount(products, .1));
+
+
+ //Task 3
+
+ let sales = [250,400, 150, 900, 1200];
+
+ function calculateTotalRevenue(sales) {
+    return sales.reduce (calculateTotalRevenue,sale)=>total+sale,runningTotal)
+ }
+
+ console.log(calculateTotalRevenue(sales)};
+
+ //Task 4
+
+ let employee = { "john Doe"}
+
+
+
+
+
